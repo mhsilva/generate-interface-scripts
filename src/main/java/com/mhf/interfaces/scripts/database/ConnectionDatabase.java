850f683a58f975bf44538583eb75a79dd6245984
mhsilva <mhenrique.fs@gmail.com>
2018-05-21 16:21:57 -0300

    Update ConnectionDatabase.java

--- src/main/java/com/mhf/interfaces/scripts/database/ConnectionDatabase.java
+++ src/main/java/com/mhf/interfaces/scripts/database/ConnectionDatabase.java
@@ -11,7 +11,7 @@ public class ConnectionDatabase {
 
         Connection conn = null;
         try {
-            String dbURL = "jdbc:oracle:thin:@10.184.57.4:1521/orq1083a.int.thomsonreuters.com";
+            String dbURL = "jdbc:oracle:thin:@URL:1521/SERVICE_NAME";
             Properties properties = new Properties();
             properties.put("user", "OGT_FTA_MX_AB");
             properties.put("password", "OGT_FTA_MX_AB");
